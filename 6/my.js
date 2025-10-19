72d33232c7a09702f7c273b17350cfeece2f7964
Unamrr <anna12way@gmail.com>
2025-10-19 16:14:09 +0300

    Update my.js

--- 6/my.js
+++ 6/my.js
@@ -3,7 +3,7 @@
 // Функция, возвращающая все базовые цены
 function getPrices() {
   return {
-    prodTypes: [100, 150, 200],
+    prodTypes: [800, 300, 100],
     prodOptions: {
       option1: 200,
       option2: 500,
