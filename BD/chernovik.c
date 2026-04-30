4ff5f39107883f2a957b67bbb0544c1808f806be
Unamrr <anna12way@gmail.com>
2026-04-30 13:17:25 +0300

    Update chernovik.c

--- BD/chernovik.c
+++ BD/chernovik.c
@@ -1,13 +1,37 @@
-CREATE TABLE employees (
-    employee_id INTEGER PRIMARY KEY,
-    first_name VARCHAR(20),
-    last_name VARCHAR(25),
-    salary REAL
-);INSERT INTO employees VALUES (120, 'Matthew', 'Weiss', 8000);INSERT INTO employees VALUES (122, 'Payam', 'Kaufling', 7900);CREATE OR REPLACE FUNCTION a_function() RETURNS VOID AS $$
-DECLARE v_salary_120 REAL;
-BEGIN
-    SELECT salary INTO v_salary_120 FROM employees WHERE employee_id = 120;
-    UPDATE employees SET salary = (SELECT salary FROM employees WHERE employee_id = 122) WHERE employee_id = 120;
-    UPDATE employees SET salary = v_salary_120 WHERE employee_id = 122;
-END;
-$$ LANGUAGE plpgsql;SELECT a_function();SELECT * FROM employees WHERE employee_id IN (120, 122);
+2321313=# CREATE or replace FUNCTION b_function () RETURNS void AS $$
+2321313$# DECLARE
+2321313$#     v_exp  integer;
+2321313$#     v_hike real;
+2321313$# BEGIN
+2321313$#     select EXTRACT(DAY FROM (NOW() - hire_date) / 365 ) into v_exp
+2321313$#     from employees where employee_id = 115;
+2321313$#
+2321313$#     v_hike := 1.05;
+2321313$#
+2321313$#     case
+2321313$#       when  v_exp > 10 then
+2321313$#             v_hike := 1.20;
+2321313$#       when  v_exp > 5  then
+2321313$#             v_hike := 1.10;
+2321313$#     end case;
+2321313$#
+2321313$#     update employees set salary = salary * v_hike
+2321313$#     where employee_id = 115;
+2321313$# END;
+2321313$#
+2321313$# $$ LANGUAGE plpgsql;
+CREATE FUNCTION
+2321313=#
+2321313=#
+2321313=#
+2321313=# SELECT b_function() AS output;
+ОШИБКА:  столбец "hire_date" не существует
+LINE 1: select EXTRACT(DAY FROM (NOW() - hire_date) / 365 )         ...
+                                         ^
+QUERY:  select EXTRACT(DAY FROM (NOW() - hire_date) / 365 )                from
+employees where employee_id = 115
+CONTEXT:  функция PL/pgSQL b_function(), строка 6, оператор SQL-оператор
+2321313=# Select  salary From employees where  employee_id = 115;
+ salary
+--------
+(0 rows)
